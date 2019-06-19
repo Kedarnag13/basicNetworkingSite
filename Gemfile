846a3c67eb18c571861756712dbb142e5f4d7c0e
RKReloaded <kedarnag138@gmail.com>
2019-06-19 23:30:59 +0800

    [development implement_page_scraping] :sparkles: Scraping the webpage using nokogiri.

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'bitly', '~> 0.10.4'
+gem 'nokogiri', '~> 1.6', '>= 1.6.8'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,12 @@
+require 'open-uri'
 class Member < ApplicationRecord
+  before_save :crawl_website
+
+  def crawl_website
+    doc = Nokogiri::HTML(open(self.original_url))
+    doc.css('h1').each do |heading|
+      p heading.text
+    end
+  end
+
 end
