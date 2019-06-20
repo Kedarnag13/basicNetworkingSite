27d3a380c1c89236fd403f264c31549f30922082
RKReloaded <kedarnag138@gmail.com>
2019-06-20 23:59:11 +0800

    [development implement_search_for_members_by_content] :sparkles: Searches members and content based on search term.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/search' => 'welcome#search', as: :search_by_content
   resources :members do
     member do
       put :add_as_friend
